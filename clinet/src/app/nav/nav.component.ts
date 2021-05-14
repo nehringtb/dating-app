e370a4889b71f844e02ff109bfdcadd6bdd09f7c
Thomas Nehring <thomas.nehrong@state.co.us>
2021-05-13 19:26:32 -0600

    End of section 7

--- clinet/src/app/nav/nav.component.ts
+++ clinet/src/app/nav/nav.component.ts
@@ -26,8 +26,7 @@ export class NavComponent implements OnInit {
       console.log(Response);
       this.router.navigateByUrl('/members');
     }, error =>{
-      console.log(error);
-      this.toastr.error(error.error);
+     
     });
   }
 
