4f21cb144621caa47f5efb5e1af6f62993076856
Thomas Nehring <thomas.nehrong@state.co.us>
2021-06-26 15:20:27 -0600

    End of section 11

--- clinet/src/app/_services/account.service.ts
+++ clinet/src/app/_services/account.service.ts
@@ -15,19 +15,20 @@ export class AccountService {
 
   constructor(private http: HttpClient) { }
 
-  login(model: any){
+  login(model: any){ 
     return this.http.post(this.baseUrl +'account/login', model).pipe(
       map((response: User) => {
         const user = response;
         if (user){
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.setCurrentUser(user);
+          
         }
       })
     );
   }
 
   setCurrentUser(user: User){
+    localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
 
@@ -39,10 +40,7 @@ export class AccountService {
   register(model: any){
     return this.http.post(this.baseUrl + 'account/register', model).pipe(
       map((user: User) => {
-        if (user){
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
-        }
+        this.setCurrentUser(user);
       })
     );
   }
