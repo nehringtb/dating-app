ebc04eda13ff83b823b99fbaec52a4d00f170d6f
Thomas Nehring <thomas.nehrong@state.co.us>
2021-08-06 19:56:36 -0600

    End of section 13

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,7 +1,9 @@
+using API.Helpers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    [ServiceFilter(typeof(LogUserActivity))]
     [ApiController]
     [Route("API/[controller]")]
     public class BaseApiController : ControllerBase
