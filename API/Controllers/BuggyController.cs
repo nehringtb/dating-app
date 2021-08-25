95340453a83f3e3d912749666937c9e6e83b1bbd
Thomas Nehring <thomas.nehrong@state.co.us>
2021-08-24 20:10:00 -0600

    End of section 14

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -44,7 +44,7 @@ namespace API.Controllers
         public ActionResult<string> GetBadRequest()
         {
             
-            return BadRequest("This was not a good request");
+            return BadRequest();
         }
    
     }
---
+++ API/Helpers/LikesParams.cs
@@ -0,0 +1,9 @@
+namespace API.Helpers
+{
+    public class LikesParams : PaginationParams
+    {
+        public int Userid { get; set; }
+
+        public string predicate { get; set; }
+    }
+}
